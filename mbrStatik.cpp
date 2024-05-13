e646694cd3e8e3e171193702364ebcb2ef33000c
febry40 <febryevayonisa@gmail.com>
2024-05-13 18:24:00 +0700

    membuat awal codingan danclass mahasiwa

---
+++ mbrStatik.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa
+{
+    public:
+       static int nim;
+       int id;
+       string nama;
+
+       void setID();
+
+       void printAll();
+       Mahasiswa(string pnama):nama(pnama){
+
+             setID();
+       }
+};
+
